8d1fad65682e7bedf92d35c83bbddb97cc110a14
Vladimir Kochnev <hashtable@yandex.ru>
2022-08-01 13:46:38 +0300

    Precise timeout support for Gana::Event#wait_until

--- lib/gana/event.rb
+++ lib/gana/event.rb
@@ -15,14 +15,31 @@ module Gana
 
     def wait_until(timeout = nil)
       @evs_mutex.synchronize do
-        @evs_cond.wait(@evs_mutex, timeout) until yield self
+        unless timeout
+          loop do
+            return true if yield self
+
+            @evs_cond.wait(@evs_mutex)
+          end
+        else
+          was = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+          loop do
+            return true if yield self
+
+            @evs_cond.wait(@evs_mutex, timeout)
+            now = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+            duration = now - was
+            return false if duration >= timeout
+
+            timeout -= duration
+            was = now
+          end
+        end
       end
     end
 
     def wait_while(timeout = nil)
-      @evs_mutex.synchronize do
-        @evs_cond.wait(@evs_mutex, timeout) while yield self
-      end
+      wait_until(timeout) { |obj| !yield obj }
     end
 
     attr_reader :state
